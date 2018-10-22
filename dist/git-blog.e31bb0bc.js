8e09a84ed1dc71c1d59c2231063598cb3bce2a89
Alessandro Muraro <akmuraro@gmail.com>
2018-10-22 16:38:45 +0200

    adding favicon

--- dist/git-blog.e31bb0bc.js
+++ dist/git-blog.e31bb0bc.js
@@ -3172,7 +3172,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52119" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50050" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
